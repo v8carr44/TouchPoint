6fc892b0670f953775d9959296ea42ef90bad060
v8carr44 <yt.zero213@gmail.com>
2026-06-25 17:08:25 +1000

    Update clearuser.js

--- src/commands/Moderation/clearuser.js
+++ src/commands/Moderation/clearuser.js
@@ -1,57 +1,58 @@
-const Discord = require('discord.js');
+const { PermissionsBitField } = require('discord.js');
 
-module.exports = async (client, interaction, args) => {
-    const perms = await client.checkPerms({
-        flags: [Discord.PermissionsBitField.Flags.ManageMessages],
-        perms: [Discord.PermissionsBitField.Flags.ManageMessages]
-    }, interaction);
+module.exports = async (client, interaction) => {
+    // Permission check
+    if (!interaction.member.permissions.has(PermissionsBitField.Flags.ManageMessages)) {
+        return client.errNormal({
+            error: "You don't have permission to manage messages!",
+            type: 'editreply'
+        }, interaction);
+    }
 
-    if (perms == false) return;
+    const user = interaction.options.getUser('user');
 
-    const member = interaction.options.getUser('user');
+    if (!user) {
+        return client.errNormal({
+            error: "You must provide a user to purge messages from!",
+            type: 'editreply'
+        }, interaction);
+    }
 
-    interaction.guild.channels.cache.forEach(async (channel) => {
-        if (channel.messages) {
-            let messages = await channel.messages.fetch();
-            let userMessages = messages.filter((m) => m.author.id === member.id);
-            await channel.bulkDelete(userMessages).then(() => {
-                client.succNormal({
-                    text: `I have successfully deleted the messages`,
-                    fields: [
-                        {
-                            name: "👤┆User",
-                            value: `${member} (${member.tag})`,
-                            inline: true
-                        }
-                    ],
-                    type: 'editreply'
-                }, interaction).then(msg => setTimeout(() => {
-                    msg.delete()
-                }, 5000));
-            }).catch(err => { });
+    try {
+        // Fetch last 100 messages in channel
+        const messages = await interaction.channel.messages.fetch({ limit: 100 });
+
+        // Filter messages from that user
+        const userMessages = messages.filter(m => m.author.id === user.id);
+
+        if (userMessages.size === 0) {
+            return client.errNormal({
+                error: "No messages found from that user in the last 100 messages.",
+                type: 'editreply'
+            }, interaction);
         }
-    });
 
-    interaction.channel.bulkDelete(amount + 1).then(() => {
-        client.succNormal({
-            text: `I have successfully deleted the messages`,
+        // Bulk delete (Discord only allows <14 day messages)
+        await interaction.channel.bulkDelete(userMessages, true);
+
+        return client.succNormal({
+            text: `Deleted ${userMessages.size} messages from ${user.username}.`,
             fields: [
                 {
-                    name: "💬┆Amount",
-                    value: amount,
+                    name: "👤 User",
+                    value: `${user.tag}`,
                     inline: true
                 }
             ],
             type: 'editreply'
-        }, interaction).then(msg => setTimeout(() => {
-            msg.delete()
-        }, 5000));
-    }).catch(err => {
-        client.errNormal({
-            error: "There was an error trying to delete messages in this channel!",
+        }, interaction);
+
+    } catch (err) {
+        console.error(err);
+
+        return client.errNormal({
+            error: "Failed to delete messages (they may be older than 14 days).",
             type: 'editreply'
         }, interaction);
-    });
-}
-
- 
+    }
+};
