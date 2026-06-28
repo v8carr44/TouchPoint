417b697c8e2d8235ed50812014bacbdab1d53b53
v8carr44 <yt.zero213@gmail.com>
2026-06-28 14:17:54 +1000

    Create streamNotifications.js

---
+++ src/config/config/streamNotifications.js
@@ -0,0 +1,53 @@
+export default {
+  // Discord
+  guildId: "", // Leave blank to work in every server the bot is in
+
+  announcementChannelId: "1519348096566300803",
+
+  pingEveryone: true,
+
+  pingUserId: "487935188798537759",
+
+  // Twitch
+  twitch: {
+    username: "v8carr4_pro",
+
+    clientId: process.env.TWITCH_CLIENT_ID,
+
+    clientSecret: process.env.TWITCH_CLIENT_SECRET,
+
+    checkInterval: 60000 // 60 seconds
+  },
+
+  // YouTube
+  youtube: {
+    channelHandle: "@The-V8-Productions",
+
+    apiKey: process.env.YOUTUBE_API_KEY,
+
+    checkInterval: 120000 // 2 minutes
+  },
+
+  embeds: {
+
+    color: 0x9146FF,
+
+    footer: {
+      text: "Touchpoint Live Notifications"
+    }
+
+  },
+
+  notifications: {
+
+    announceStreams: true,
+
+    announceVideos: true,
+
+    announceShorts: false,
+
+    announcePremieres: true
+
+  }
+
+};
